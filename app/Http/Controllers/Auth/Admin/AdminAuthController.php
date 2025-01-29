ffce03d9de406824572957927d3e8c12de69bec1
nadaeezzdin <nadaeezzdin@gmail.com>
2025-01-29 03:14:37 +0200

    create admin auth

---
+++ app/Http/Controllers/Auth/Admin/AdminAuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Services\AuthService;
+use App\Http\Requests\Auth\LoginRequest;
+
+
+class AdminAuthController extends Controller
+{
+  protected $authService;
+
+  public function __construct(AuthService $authService)
+  {
+    $this->authService = $authService;
+  }
+
+  public function show_login()
+  {
+    return view('admin.auth.login');
+  }
+
+
+  public function login(LoginRequest $request)
+  {
+    if ( $this->authService->login($request)) {
+      return redirect()->route('admin.home');
+    }
+  }
+
+  public function logout(Request $request)
+  {
+    return $this->authService->logout($request);
+  }
+}
