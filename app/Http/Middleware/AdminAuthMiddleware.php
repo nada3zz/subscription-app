3b6e9e730ebb15c6b5f2623a2d11988c7b5d2d92
nadaeezzdin <nadaeezzdin@gmail.com>
2025-02-01 02:32:00 +0200

    add admin model& middleware

--- app/Http/Middleware/AdminAuthMiddleware.php
+++ app/Http/Middleware/AdminAuthMiddleware.php
@@ -4,8 +4,9 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-class RoleMiddleware
+class AdminAuthMiddleware
 {
     /**
      * Handle an incoming request.
@@ -14,10 +15,10 @@ class RoleMiddleware
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next, $role)
+    public function handle(Request $request, Closure $next)
     {
-        if ($request->user() && $request->user()->role !== $role) {
-            return response()->json(['message' => 'Unauthorized'], 403);
+        if (!Auth::guard('admin')->check()) {
+            return redirect()->route('admin.login');
         }
 
         return $next($request);
---
+++ app/Models/Admin/Admin.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models\Admin;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Admin extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'password',
+        'is_active',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+}
